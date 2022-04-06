d957a9e138403281cd51385ca1ec4c4a858bc8dd
Evan <evfernberg@gmail.com>
2022-04-06 18:31:19 -0400

    updated to new react-router syntax

--- src/App.js
+++ src/App.js
@@ -1,45 +1,40 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  Link,
+} from 'react-router-dom'
 import './App.css';
+// import logo from './logo.svg';
+import { HomePage } from './components/Home.page'
+import { Players } from './components/Players.page'
+import { RQPlayers } from './components/RQPlayers.page'
 
 function App() {
-
-  // Function to add our give data into cache
- const addDataIntoCache = (cacheName, url, response) => {
-   // Converting our response into Actual Response form
-   const data = new Response(JSON.stringify(response));
-
-   if ('caches' in window) {
-     // Opening given cache and putting our data into it
-     caches.open(cacheName).then((cache) => {
-       cache.put(url, data);
-       alert('Data Added into cache!')
-     });
-   }
- };
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <h4>How to store data into cache in ReactJS?</h4>
-        <button onClick={()=>addDataIntoCache('MyCache',
-        'https://localhost:300','SampleData')} >
-        Add Data Into Cache
-        </button>
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-      </header>
-    </div>
-  );
-}
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/players'>Patriots Legends</Link>
+            </li>
+            <li>
+              <Link to='/rq-players'>RQ Patriots Legends</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/players' element={<Players />} />
+          <Route path='/rq-players' element={<RQPlayers />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  )
+};
 
 export default App;
